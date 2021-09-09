9e2f4c24436c0a94f16090c50cb4215deae24db1
Hernán Felipe Serna Gómez <hfserna@gmail.com>
2021-09-08 22:31:23 -0500

    [REFACTOR] Refactorizar componente Listado.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 import Button from "./components/Button";
-import Listado from './components/Listado';
+import Listado from "./components/Listado";
 
 function App() {
-  //Necesarias tantas variables??
-  let fraseBotonDefault = "Haz Click";
+
+  const frases = ["Haz Click", "Click si eres valiente", "Prueba otra frase", "Yo controlo el boton"];
+
+  /*let fraseBotonDefault = "Haz Click";
   let fraseBotonUno = "Click si eres valiente";
   let fraseBotonDos = "Prueba otra frase";
-  let fraseBotonTres = "Yo controlo el boton";
+  let fraseBotonTres = "Yo controlo el boton";*/
 
-// Funcion que obtiene una frase del boton, espera un arreglo... REFACTORIZAR 
-  function fraseRandom(arreglo = [], fraseActual, fraseUno, fraseDos, fraseTres) {
+  // Funcion que obtiene una frase del boton, espera un arreglo... REFACTORIZAR
+  function fraseRandom(
+    arreglo = [],
+    fraseActual,
+    fraseUno,
+    fraseDos,
+    fraseTres
+  ) {
     let indice = Math.floor(Math.random() * 4); // Numero random
     let arregloDeFrases = [fraseActual, fraseUno, fraseDos];
     arregloDeFrases.push(fraseTres);
@@ -20,15 +28,17 @@ function App() {
       indice = Math.floor(Math.random() * 4);
     }
     fetchData();
-    return arregloDeFrases[indice]
+    return arregloDeFrases[indice];
   }
 
-  const [fraseChuck, setFraseChuck] = useState("") // Manera de react para crear estados
+  const [fraseChuck, setFraseChuck] = useState(""); // Manera de react para crear estados
 
   async function fetchData() {
-    const resultado = await axios.get('https://api.chucknorris.io/jokes/random')
-    setFraseChuck(resultado.data.value) // Mandera de react para actualizar estados
-  };
+    const resultado = await axios.get(
+      "https://api.chucknorris.io/jokes/random"
+    );
+    setFraseChuck(resultado.data.value); // Mandera de react para actualizar estados
+  }
 
   return (
     <div className="app">
@@ -42,9 +52,7 @@ function App() {
       />
       <Listado listadoDeFrases={fraseChuck} />
     </div>
-
-  )
-
+  );
 }
 
 export default App;
--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -8,17 +8,10 @@ const Listado = ({listadoDeFrases}) => {
         setFrasesState([...frasesState, listadoDeFrases])
     }, [listadoDeFrases])
 
-
-    // EJERCICIO PARA REFACTORIZAR DESPUES DEL RETURN
     return (
         <div className="app__viejasFrases">
         <h3>ULTIMAS 5 FRASES</h3>
-        {frasesState[0] ? <p>{frasesState[0]}</p> : null}
-        {frasesState[1] ? <p>{frasesState[1]}</p> : null}
-        {frasesState[2] ? <p>{frasesState[2]}</p> : null}
-        {frasesState[3] ? <p>{frasesState[3]}</p> : null}
-        {frasesState[4] ? <p>{frasesState[4]}</p> : null}
-        {frasesState[5] ? <p>{frasesState[5]}</p> : null}
+        {frasesState.map((frase, i) => i <= 5? <p key={i}>{frase}</p> : null)}
       </div>
     )
 }
